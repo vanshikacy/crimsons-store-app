67573e997d648a94b6140dc2ecc99bcf533608e3
vanshikacy <119793430+vanshikacy@users.noreply.github.com>
2025-08-17 16:44:16 +0530

    routing issues fix

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,26 +1,27 @@
-import Stripe from "stripe"
-import '../../../envConfig.js'
+import Stripe from "stripe";
+import '../../../envConfig.js';
 
-//initialising stripe to a new instance of class Stripe
-//that we just imported
-const API_KEY=process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
-const stripe=new Stripe(API_KEY)
+export const dynamic = "force-dynamic"; 
 
-export async function POST(request){
-    try{
-        const {lineItems} = await request.json()
-        console.log(lineItems)
-        const session=await stripe.checkout.sessions.create({
-            mode: 'payment',
-            line_items: lineItems,
-            success_url: process.env.NEXT_PUBLIC_BASE_URL + '/success',
-            cancel_url:process.env.NEXT_PUBLIC_BASE_URL+ '/'
-        })
-        return Response.json(session)
+export async function POST(request) {
+  try {
+    const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY; 
+    const stripe = new Stripe(API_KEY); 
 
-    } catch (err) {
-     //end points need to respond even if there is an error
-        console.error('Error creating cart checkout:', err.message)
-        return Response.json({error: 'Failed to create stripe checkout page'})
-    }
+    const { lineItems } = await request.json();
+    console.log(lineItems);
+
+    const session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      line_items: lineItems,
+      success_url: process.env.NEXT_PUBLIC_BASE_URL + '/success',
+      cancel_url: process.env.NEXT_PUBLIC_BASE_URL + '/'
+    });
+
+    return Response.json(session);
+
+  } catch (err) {
+    console.error('Error creating cart checkout:', err.message);
+    return Response.json({ error: 'Failed to create stripe checkout page' });
+  }
 }
